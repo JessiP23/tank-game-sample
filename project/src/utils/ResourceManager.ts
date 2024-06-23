522c59cf00bc6885184e5aee0e5160e33e7551d0
Jessi Pavia <jessi316866@gmail.com>
2024-06-23 13:47:36 -0400

    texture for tank loaded

--- project/src/utils/ResourceManager.ts
+++ project/src/utils/ResourceManager.ts
@@ -1,4 +1,5 @@
 import { Texture, TextureLoader } from "three";
+import { GLTF, GLTFLoader } from "three/examples/jsm/Addons.js";
 
 class ResourceManager{
     private static _instance = new ResourceManager()
@@ -9,13 +10,44 @@ class ResourceManager{
 
     //resource list
     private _groundTextures: Texture[] =[];
+    private _models = new Map<string, GLTF>()
+    private _textures = new Map<string, Texture>();
+
+    //public methods to access games loaded resources
+    public getModel(modelName:string): GLTF | undefined {
+        return this._models.get(modelName);
+    }
+
+    public getTexture(textureName:string): Texture | undefined {
+        return this._textures.get(textureName);
+    }
 
     //load entry point
     public load = async () => {
         //create a unique texture loader
         const textureLoader = new TextureLoader();
         await this.loadGroundTextures(textureLoader);
+        await this.loadTextures(textureLoader);
+        await this.loadModels();
     };
+
+    private loadModels = async () => {
+        //instance a model loader
+        const modelLoader = new GLTFLoader();
+        const playerTank = await modelLoader.loadAsync("models/tank.plb");
+        this._models.set('tank', playerTank);
+    }
+
+    private loadTextures = async(textureLoader:TextureLoader) => {
+        //load game textures
+        // player tank
+        const tankBodyTexture = await textureLoader.loadAsync('textures/tank-body.png');
+        const tankTurrentTexture = await textureLoader.loadAsync("textures/tank-turrent.png");
+
+        //add to the game resources
+        this._textures.set('tank-body', tankBodyTexture);
+        this._textures.set('tank-turret', tankTurrentTexture);
+    }
 
     //method for gorund textures loading
     private loadGroundTextures = async (textureLoader: TextureLoader) => {
